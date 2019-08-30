17b04c17bb9e56b0b8cbe005aa0cc14c4c77ba32
Achyuth Chandra Annakula <achyuth.chandra@zemosolabs.com>
2019-08-30 14:24:19 +0530

    final commit

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let dataObject;
 $(function() {
-    if (!localStorage.getItem("data") || !dataObject || !dataObject.tables || !dataObject.items) {
+    $("#modal").modal({
+        onOpenStart: function(event) {
+            console.log(event)
+        }
+    });
+    if (!localStorage.getItem("data")) {
         $.getJSON("./data.json", function(data) {
             dataObject = data;
-
             localStorage.setItem("data", JSON.stringify(data));
             getReady();
         })
@@ -18,39 +22,203 @@ function getReady() {
     getTablesReady();
 }
 
+function allowDrop(e) {
+    e.preventDefault();
+}
+
+function drag(e) {
+    e.dataTransfer.setData("id", e.target.id);
+}
+
+function drop(e) {
+    e.preventDefault();
+    let itemId = e.dataTransfer.getData("id");
+    let index = e.path.findIndex(obj => obj.dataset.type && obj.dataset.type == "table")
+    let tableId = e.path[index].id;
+    let itemObj = dataObject.items.find(obj => obj.id == itemId);
+    let tableObj = dataObject.tables.find(obj => obj.id == tableId);
+    console.log(itemId, index, e.path)
+    if (!tableObj.items[itemId])
+        tableObj.total_items += 1;
+    tableObj.items[itemId] = tableObj.items[itemId] ? tableObj.items[itemId] + 1 : 1;
+    tableObj.total_amount += itemObj.cost;
+    changeATable(tableObj);
+    localStorage.setItem("data", JSON.stringify(dataObject));
+}
+
+function onMenuSearchChange() {
+    let searchKey = $(this).val();
+    if (searchKey != "") {
+        $("#menu-search-close").show();
+    } else {
+        $("#menu-search-close").hide();
+    }
+    $("#menu-body").html(getMenuBody(searchKey));
+}
+
+function onTableSearchChange() {
+    let searchKey = $(this).val();
+    if (searchKey != "") {
+        $("#tables-search-close").show();
+    } else {
+        $("#tables-search-close").hide();
+    }
+    $("#tables-body").html(getTablesBody(searchKey));
+}
+
+function changeATable(tableObj) {
+    $("#" + tableObj.id).html(`
+    <div class="card-panel">
+        <h5>${tableObj.name}</h5>
+        <span>Rs.${tableObj.total_amount} | Total items: ${tableObj.total_items}</span>
+    </div>`);
+}
+
+function clearMenuSearch() {
+    $("#menu-search").val("");
+    $("#menu-search").trigger("input");
+}
+
+function clearTablesSearch() {
+    $("#tables-search").val("");
+    $("#tables-search").trigger("input");
+}
+
+function getItem(id) {
+    return dataObject.items.find(obj => obj.id == id);
+}
+
+function getTable(id) {
+    return dataObject.tables.find(obj => obj.id == id);
+}
+
+function deleteItemFromTable(tableId, itemId) {
+    let tableObj = getTable(tableId);
+    tableObj.total_items -= 1;
+    let count = tableObj.items[itemId];
+    tableObj.items[itemId] = undefined;
+    tableObj.total_amount -= getItem(itemId).cost * count;
+    localStorage.setItem("data", JSON.stringify(dataObject));
+    changeATable(tableObj);
+    updateModalBody(tableObj);
+}
+
+function closeSession(tableId) {
+    console.log(event);
+    dataObject.tables.forEach(obj => {
+        if (obj.id == tableId) {
+            obj.total_items = 0;
+            obj.total_amount = 0;
+            obj.items = {};
+            changeATable(obj);
+        }
+    })
+    localStorage.setItem("data", JSON.stringify(dataObject));
+}
+
+function updateModalBody(tableObj) {
+    let out = "";
+    let tableId = tableObj.id;
+    if (tableObj.total_items == 0) {
+        out = `<div class="empty-table">Empty Order</div>`;
+    } else {
+        out = `<table class="highlight">
+            <thead class="table-head">
+                <tr>
+                    <th>S.No</th>
+                    <th>Item</th>
+                    <th>Price</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>`;
+        //console.log(tableObj)
+        let index = 1;
+        for (let itemId in tableObj.items) {
+            let item = getItem(itemId);
+            let count = tableObj.items[itemId];
+            if (!count) continue;
+            console.log(item.cost, count)
+            out += `<tr>
+                <td>${index++}</td>
+                <td>${item.name}</td>
+                <td>${item.cost * count}</td>
+                <td>
+                    <div class="input-field" >
+                        <input type="number" value="${count}" id="in${itemId}" min=1 required>
+                        <label class="active" for="in${itemId}">Number of Serving</label>
+                    </div>
+                </td>
+                <td><i class="material-icons right cursor-pointer delete" onclick="deleteItemFromTable(${tableId}, ${itemId})">delete</i></td>
+            </tr>`;
+        }
+        out += `<tr id="total">
+                <td></td>
+                <td></td>
+                <td>Total: ${tableObj.total_amount}</td>
+            </tr>
+        </tbody>
+        </table>`;
+    }
+    $("#modal-body").html(out);
+    $("#close-session").attr("onclick", `closeSession(${tableId})`);
+}
+
+function openModal(e) {
+    let index = e.path.findIndex(obj => obj.dataset.type && obj.dataset.type == "table")
+    let tableId = e.path[index].id;
+    let tableObj = dataObject.tables.find(obj => obj.id == tableId)
+        //console.log(tableId);
+    $("#modal-head-name").html(tableObj.name);
+    updateModalBody(tableObj);
+    $("#modal").modal("open");
+}
+
+function getTablesBody(searchKey) {
+    let out = ""
+    searchKey = searchKey.toLowerCase();
+    dataObject.tables.forEach(obj => {
+        if (obj.name.toLowerCase().includes(searchKey))
+            out += `<div class="col s6 m12" data-type="table" id="${obj.id}" onclick="openModal(event)" ondrop="drop(event)" ondragover="allowDrop(event)">
+                <div class="card-panel">
+                    <h5>${obj.name}</h5>
+                    <span>Rs.${obj.total_amount} | Total items: ${obj.total_items}</span>
+                </div>
+            </div>`;
+    });
+    return out;
+}
+
+function getMenuBody(searchKey) {
+    let out = "";
+    searchKey = searchKey.toLowerCase();
+    dataObject.items.forEach(obj => {
+        if (obj.type.toLowerCase().includes(searchKey) || obj.name.toLowerCase().includes(searchKey))
+            out += `<div class="col s6 m4">
+                <div id="${obj.id}" class="card-panel" draggable="true" ondragstart="drag(event)">
+                    <span class="food-type">${obj.type}</span>
+                    <h5>${obj.name}</h5>
+                    <span>Rs.${obj.cost}</span>
+                </div>
+            </div>`;
+    });
+    return out;
+}
 async function getMenuReady() {
     const menu = $("#menu");
     let out = `<div class="col s12">
         <h6 class="center-align">Menu</h6>
         <div class="card-panel search">
             <input id="menu-search" type="text" placeholder="search" style="display: inline">
-            <i id="menu-search-close" class="tiny material-icons close">close</i>
+            <i id="menu-search-close" onclick="clearMenuSearch()" class="tiny material-icons close">close</i>
         </div>
-    </div>`;
+    </div><div id="menu-body">`;
 
-    dataObject.items.forEach(obj => {
-        // draggable="true" ondragstart="drag(event)"
-        out += `<div class="col s6 m4">
-            <div id="${obj.id}" class="card-panel item">
-                <span class="food-type">${obj.type}</span>
-                <h5>${obj.name}</h5>
-                <span>Rs.${obj.cost}</span>
-            </div>
-        </div>`;
-    });
+    out += getMenuBody("") + "</div>";
 
     menu.html(out);
-
-    dataObject.items.forEach(obj => {
-        $("#" + obj.id).draggable({
-            start: function(event, ui) {
-                $(this).css("z-index", 12);
-            },
-            helper: "clone",
-            cursor: "move",
-            scroll: false
-        });
-    })
+    $('#menu-search').on('input', onMenuSearchChange);
 }
 
 async function getTablesReady() {
@@ -58,54 +226,13 @@ async function getTablesReady() {
     let out = `<div class="col s12 center-align">
         <h6>Tables</h6>
         <div class="card-panel search">
-            <input id="table-search" type="text" placeholder="search">
-            <i id="menu-search-close" class="tiny material-icons close">close</i>
+            <input id="tables-search" type="text" placeholder="search">
+            <i id="tables-search-close" onclick="clearTablesSearch()" class="tiny material-icons close">close</i>
         </div>
-    </div>`;
+    </div><div id="tables-body">`;
 
-    dataObject.tables.forEach(obj => {
-        // ondrop="drop(event)" ondragover="allowDrop(event)
-        out += `<div class="col s6 m12">
-            <div class="card-panel table" id="${obj.id}">
-                <h5>${obj.name}</h5>
-                <span>Rs.${obj.total_amount} | Total items: ${obj.items.length}</span>
-            </div>
-        </div>`;
-    });
+    out += getTablesBody("") + "</div>";
+
     tables.html(out);
-
-    dataObject.tables.forEach(obj => {
-        $("#" + obj.id).droppable({
-            drop: function(event, ui) {
-                console.log($(ui.draggable).attr("id"), $(this).attr("id"))
-                $(this).css("background-color", "")
-            },
-            activeClass: "ui-state-default",
-            hoverClass: "ui-state-hover",
-            accept: ".item",
-            tolerance: "pointer",
-            over: function(event, ui) {
-                $(this).css("background-color", "aliceblue")
-            },
-            out: function(event, ui) {
-                $(this).css("background-color", "")
-            }
-        });
-    })
-}
-
-function allowDrop(e) {
-    e.preventDefault();
-}
-
-function drag(e) {
-    console.log(e)
-        //e.dataTransfer.setData("id", e.target.id);
-}
-
-function drop(e) {
-    e.preventDefault();
-    let data = e.dataTransfer.getData("id");
-    console.log("data", data, e.path);
-    //e.target.appendChild(document.getElementById(data));
+    $('#tables-search').on('input', onTableSearchChange);
 }
